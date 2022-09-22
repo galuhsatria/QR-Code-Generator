099c6ce85e1b892e429bead3baf9ccbac5c30976
galuhsatriaa <galuhsatria219@gmail.com>
2022-09-22 21:08:35 +0800

    commit file

---
+++ script.js
@@ -0,0 +1,18 @@
+const wrapper = document.querySelector(".wrapper"),
+  textInput = wrapper.querySelector(".form input"),
+  generateBtn = wrapper.querySelector(".form button"),
+  qrImg = wrapper.querySelector(".qr-img img");
+
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+  let qrValue = textInput.value.trim();
+  if (!qrValue || preValue === qrValue) return;
+  preValue = qrValue;
+  generateBtn.innerText = "Generating QR Code...";
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+  qrImg.addEventListener("load", () => {
+    wrapper.classList.add("active");
+    generateBtn.innerText = "Generate QR Code";
+  });
+});
