d1a20cf4063ef96e4d02a38bd6025aac00964c82
galuhsatriaa <galuhsatria219@gmail.com>
2022-09-23 12:47:36 +0800

    add button download

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const wrapper = document.querySelector(".wrapper"),
   qrImg = wrapper.querySelector(".qr-img img");
 
 let preValue;
-
+const btnDownload = document.querySelector(".btn-download");
 generateBtn.addEventListener("click", () => {
   let qrValue = textInput.value.trim();
   if (!qrValue || preValue === qrValue) return;
   preValue = qrValue;
   generateBtn.innerText = "Generating QR Code...";
   qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+  btnDownload.setAttribute("href", `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`);
   qrImg.addEventListener("load", () => {
     wrapper.classList.add("active");
     generateBtn.innerText = "Generate QR Code";
